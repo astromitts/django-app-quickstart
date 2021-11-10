11ecae62ca1eea65dc4e51652a64ce1f17e4d9ed
Bo Morin <morinbe@gmail.com>
2021-11-10 10:22:27 -0500

    Add a helper app to hold middleware and context processors and feature flags

--- settings/__init__.py
+++ settings/__init__.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_summernote',
     'rest_framework',
+    'helpers',
     'appuser',
 ]
 
@@ -51,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'appuser.appuser_middleware.session_request_validation',
+    'helpers.middleware.session_request_validation',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -69,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'helpers.context_processor.context_processor',
             ],
         },
     },
